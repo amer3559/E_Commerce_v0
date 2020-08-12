057b00b70b79c74d9270dbff8e699bafb6621497
Amer_Morsy <amer.morsy2015@gmail.com>
2020-08-12 21:03:50 +0200

    setup and preparing

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php
+    include 'init.php';
+    include $tpl . 'header.php';
+    include $lang . 'english.php';
+?>
+<h1>Welcome to Admin Index Page </h1>
+<p class="lead"> <?php echo lang('MEMBERS'); ?></p>
+
+
+<?php include $tpl . 'footer.php'; ?>
---
+++ admin/init.php
@@ -0,0 +1,9 @@
+<?php
+
+	// Routes
+
+	$tpl 	= 'includes/templates/'; // Template Directory
+	$lang 	= 'includes/languages/'; // Language Directory
+	$func	= 'includes/functions/'; // Functions Directory
+	$css 	= 'layout/css/'; // Css Directory
+	$js 	= 'layout/js/'; // Js Directory
